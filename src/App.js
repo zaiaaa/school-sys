c234a385923a62895595c4149cf34c854a264cbc
zaiaaa <gustavo.0801214@gmail.com>
2023-12-10 18:56:49 -0300

    preparing home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './global.css';
 
 import {
   BrowserRouter as Router,
@@ -6,6 +6,7 @@ import {
   Routes, 
   Link
 } from 'react-router-dom'
+import { Home } from './pages/home/home';
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
     <div className="App">
        <Router>
         <Routes>
-          <Route></Route>
+          <Route path='/' element={<Home />}></Route>
         </Routes>
        </Router>
     </div>
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Home = () => {
+    document.title = 'EduSIS | Home'
+    
+    return(
+        <div>Olá Home</div>
+    )
+}
+
+export {Home}
