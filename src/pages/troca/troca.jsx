38f8ae10aa686572e35d972e3bf8b09542a4ac65
zaiaaa <gustavo.0801214@gmail.com>
2024-07-12 10:34:00 -0300

    ta alterando senha

--- src/pages/troca/troca.jsx
+++ src/pages/troca/troca.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { useParams } from 'react-router-dom';
-
+import { api } from '../../services/api';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/auth';
 
 const Troca = () => {
-    const {rm} = useParams();
-  
+  const {rm} = useParams();
+  const {logoff} = useContext(AuthContext)
+  const [passwordChanged, setPasswordChanged] = useState(false)
+  const [newPassword, setNewPassword] = useState("")
+
+
+  const handleClickSubmit = async () => {
+    alert('enviou a PORRA do formulário')
+    try{
+      await api.put(`/users/alt/${rm}`, {
+        password: newPassword
+      })
+      setPasswordChanged(true)
+      alert('Senha alterada com sucesso! retorne a tela de login')
+    }catch(e){
+      alert(e)
+    }
+  }
+
    return (
+    <>
     <div>Troca de senha do {rm}</div>
+
+    <form onSubmit={handleClickSubmit}>
+      <input type="text" name="senhaNova" id="" value={newPassword} onChange={(e) => setNewPassword(e.target.value)}
+      />
+      <button type="submit">Trocar a senha</button>
+    </form>
+
+    {passwordChanged === true ?
+     <Link to={'/login'}>Volte a tela de login</Link>
+     :
+     
+     ""}
+    
+    </>
   )
 }
 
